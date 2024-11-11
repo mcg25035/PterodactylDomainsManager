de2cc8b046729960263a96989a64f194808360f8
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-11-11 22:08:53 +0800

    Create upstreamApi.js

---
+++ src/utils/upstreamApi.js
@@ -0,0 +1,10 @@
+// src/utils/upstreamApi.js
+// 此模組用於與上游網域商的 API 對接
+// 目前暫未實作
+
+module.exports = {
+    // createSubdomain: async (fullDomain, targetIp, targetPort) => { /* 實作 */ },
+    // updateSubdomain: async (fullDomain, targetIp, targetPort) => { /* 實作 */ },
+    // deleteSubdomain: async (fullDomain) => { /* 實作 */ },
+  };
+  
