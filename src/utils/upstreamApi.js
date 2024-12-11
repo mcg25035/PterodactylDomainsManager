ea4392be0991ed307eac6773e1131b0ca53bc62b
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-12-11 14:40:21 +0800

    add debug code

--- src/utils/upstreamApi.js
+++ src/utils/upstreamApi.js
@@ -91,6 +91,8 @@ const createSrvRecord = async (recordName) => {
  */
 const updateSrvRecord = async (existingRecord) => {
     const srvName = existingRecord.name; // Should be _minecraft._tcp.mc0001
+    console.log(contentParts);
+    console.log("debug");
     const contentParts = existingRecord.content.split(' ');
     if (contentParts.length < 4) {
         throw new Error(`Invalid SRV record content: ${existingRecord.content}`);
